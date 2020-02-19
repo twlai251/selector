8c16ff5c811d56d2953022cf885031bdcb046b09
twlai251 <twlai251@gmail.com>
2020-02-18 20:41:26 -0500

    Added the shape component and tested the onClick functionallity, app fully works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Shape from './shape';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Selector extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedShape: 'square',
+    }
+  }
+
+  //our methods live here
+
+  selectShape = (shapeName) => {
+    this.setState({
+      selectedShape: shapeName,
+    })
+  }
+
+
+
+  render () {
+    return(
+      <div className="container">
+
+        <div className="navbar">
+
+          <div>Selected: <span>{this.state.selectedShape}</span> </div>
+
+        </div>
+
+        <div className="shape-list">
+
+          <Shape shape="square" selectShape={this.selectShape}/>
+          <Shape shape="circle" selectShape={this.selectShape}/>
+          <Shape shape="triangle" selectShape={this.selectShape}/>
+
+        </div>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Selector;
